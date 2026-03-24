e86b7940b4f422d7c2302ed72b971d79d96ba419
fajar-dev <fajarrivaldi2015@gmail.com>
2026-03-24 15:06:08 +0700

    init: progressif web app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,11 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2025-07-15',
 
-  modules: ["nuxt-charts", "nuxt-toast"],
+  modules: [
+    "nuxt-charts",
+    "nuxt-toast",
+    "@vite-pwa/nuxt",
+  ],
 
   devtools: { enabled: true },
 
@@ -18,6 +22,60 @@ export default defineNuxtConfig({
     settings: {
       position: 'topRight',
       timeout: 3000,
+    }
+  },
+
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: "Kawan Nusa",
+      short_name: "Kawan Nusa",
+      description: "Portal Referral PT. Media Antar Nusa",
+      lang: 'id',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#629E43',
+      icons: [
+        {
+          src: '/icons/icon_64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/icon_144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/icon_192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+
+          src: '/icons/icon_384x384.png',
+          sizes: '384x384',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/icon_512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      cleanupOutdatedCaches: true
+    },
+    client: {
+      installPrompt: true,
+      periodicSyncForUpdates: 3600,
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module',
     }
   },
 
